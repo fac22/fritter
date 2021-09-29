18743cd90b0dd721634991a8ba8301e79ffd371d
jihye <jiji.pak41@gmail.com>
2021-09-29 16:47:46 +0100

    test second try

---
+++ cypress/integration/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+it('can navigate pages', () => {
+  cy.visit('/');
+});
+
+it('adds a name', () => {
+  cy.visit('/');
+  cy.get('form').find("input[name='message']").type('Blah blah');
+  cy.get('form').find("input[name='name']").type('Anna');
+  cy.contains('Send').click();
+  cy.contains('Anna');
+});
+
+it('can delete a messsage', () => {
+  cy.get('form[action="/deletefrit"]').find('button').click();
+  cy.contains('Alex').should('not.exist');
+});
+
+it('posts annoymousely if no name provided', () => {
+  cy.get('form').find("input[name='message']").type('New Frit');
+  cy.get('form').find("input[name='name']");
+  cy.contains('Send').click();
+  cy.contains('annonymouse 🐭');
+});
