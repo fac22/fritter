faf1497a267088dbfc5b12ea62bcc72442fe5117
“Moggach” <acunnane13@gmail.com>
2021-10-01 11:29:48 +0100

    git commit -m 'adding messageHTMLBuilder module - refactoring Server.js'
    Co-authored-by: cemalokten 60609268+cemalokten@users.noreply.github.com
    Co-authored-by: lopezelpesado 76691426+lopezelpesado@users.noreply.github.com
    Co-authored-by: jijip41 78619809+jijip41@users.noreply.github.com

--- server.js
+++ server.js
@@ -5,53 +5,15 @@ const express = require('express');
 const frits = require('./frits.js');
 const html = require('./body.js');
 const server = express();
+const messageHTMLBuilder = require('./messageHTMLBuilder');
 
 // Static file location
 server.use(express.static('./public'));
 
 // Home root GET request (when client visits site)
 server.get('/', (request, response) => {
-  let messages = '';
-  // Loop through messages Object
-  // Apply HTML (with Object.values i.e ${frit.message}) and insert into HTML function
-  messages = Object.values(frits)
-    .map(
-      (frit) => `
-    <article class="flex flex--column border padding-2rem background--grey margin-top-2rem">
-    <div class="
-          flex
-          flex--row
-          flex--justify-space-between
-          flex--align-items-center
-        ">
-      <div class="flex flex--row flex--align-items-center">
-        <img src="https://thispersondoesnotexist.com/image" alt="profile picture of ${
-          frit.user
-        }"/>
-        <h2>${frit.user}</h2>
-      </div>
-      <time>${frit.time.toISOString().slice(0, 10)}</time>
-    </div>
-    <div class="flex flex--row flex--justify-space-between">
-      <p class="padding-top-bottom-1rem ">
-      ${frit.message}
-      </p>
-      <form action="/deletefrit" method="POST" class="flex--align-self-end">
-        <button name="deleteButton" value="${
-          frit.time
-        }" aria-label="Delete Frit"
-          class="dustbin box__button">🗑</button>
-      </form>
-    </div>
-  </article>
-
-    `
-    )
-    .reverse()
-    .join('');
-
   // Call html function with full message string
-  response.send(html(messages));
+  response.send(html(messageHTMLBuilder(frits)));
 });
 
 // Middleware - collect post in entirety
